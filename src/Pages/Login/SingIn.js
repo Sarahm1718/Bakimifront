f9228018376a3c5470305af6c51c084d412c7040
saracarohdm <saracarohdm@gmail.com>
2021-07-19 14:00:31 -0500

    se modifico sigin

--- src/Pages/Login/SingIn.js
+++ src/Pages/Login/SingIn.js
@@ -3,35 +3,11 @@ import Login from "./index";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, signInWithGoogle } from "../../functions/firebaseAuth";
 import { useHistory } from "react-router-dom";
-//import { environment} from '../../environment/backendurl'
 
 function SignIn() {
   const history = useHistory();
   const [user] = useAuthState(auth);
 
- /* const createUser = () => {
-    const HOST_API = environment.apiRegisterUser;
-
-    const user = {
-      id: auth.currentUser.uid,
-      name: auth.currentUser.displayName,
-      email: auth.currentUser.email,
-    };
-
-    fetch(HOST_API, {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((response) => console.log("hola"));
-  };
-
-  if (user) {    
-    createUser();
-  }*/
   return (
     <div className="App">
       <section>
